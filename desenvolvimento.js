a5465925f5df1e36373399a4a331a64f34803734
Lysus4k4Q <00001123445515sp@al.educacao.sp.gov.br>
2024-05-30 21:42:18 -0300

    Update desenvolvimento.js

--- desenvolvimento.js
+++ desenvolvimento.js
@@ -13,28 +13,25 @@
 // ladrões de bicicleta, 12, drama
 
 // o menino que descobriu o vento, 14, drama
-
-let campoIdade;
-let campoFantasia;
-
 function setup() {
-    createCanvas(800, 400);
-    createSpan("Sua idade?")
-    campoIdade = createInput("15");
-    campoFantasia = createCheckbox("Gosta de Fantasia ?");
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
 }
 
 // Código omitido
 
-
-// código omitido
-
 function draw() {
-    background("#E73F3F");
-    let idade = campoIdade.value();
-    let gostaDeFantasia = campoFantasia.checked();
-    let recomendacao = geraRecomendacao(idade,
-gostaDeFantasia);
+  background("rgb(235,128,128)");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
     fill(color(30, 78, 115))
     textAlign(CENTER, CENTER)
     textSize(38);
@@ -45,17 +42,27 @@ gostaDeFantasia);
 // código omitido
 
 
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
 
-function geraRecomendacao(idade, gostaDeFantasia) {
+// Trecho de código suprimido
+
     if(idade >= 10) {
         if(idade >= 14) {
             return "O menino que descobriu o vento";
         } else {
+          if(idade >= 12) {
+            if(gostaDeFantasia || gostaDeAventura){
+              return "Homen-aranha: no aranhaverso"              
+            } else {                       
+            return "Ladrões de bicicleta"
+            }
+            } else {
             if(gostaDeFantasia){
                 return "As aventuras de Pi";
             } else {
                 return "Depois da chuva";
             }
+          }
         }
     } else {
         if(gostaDeFantasia) {
